ab6ecece584d2e9c04e30ee49073a2f504ad1ae4
devdanco <devdanco@gmail.com>
2023-12-13 10:41:52 +0100

    feat: add new rpc
    [only @mangata-finance/type-definitions]

--- packages/type-definitions/src/index.ts
+++ packages/type-definitions/src/index.ts
@@ -216,6 +216,50 @@ export const mRpc = {
       params: [],
       type: "Vec<TokenId>"
     }
+  },
+  pos: {
+    calculate_native_rewards_amount: {
+      description: "Calculates amount of available native rewards",
+      params: [
+        {
+          name: "account",
+          type: "AccountId"
+        },
+        {
+          name: "liquidity_token",
+          type: "TokenId"
+        }
+      ],
+      type: "Balance"
+    },
+    calculate_3rdparty_rewards_amount: {
+      description: "Calculates amount of available 3rdparty rewards",
+      params: [
+        {
+          name: "account",
+          type: "AccountId"
+        },
+        {
+          name: "liquidity_token",
+          type: "TokenId"
+        },
+        {
+          name: "rewards_token",
+          type: "TokenId"
+        }
+      ],
+      type: "Balance"
+    },
+    calculate_3rdparty_rewards_all: {
+      description: "Calculates all amount of available 3rdparty rewards",
+      params: [
+        {
+          name: "account",
+          type: "AccountId"
+        }
+      ],
+      type: "Balance"
+    }
   }
 };
 
