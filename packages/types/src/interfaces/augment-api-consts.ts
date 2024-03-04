aca53fc66666c7b1316fbb560a907e11a57e62ff
mangatafinance <solutions@mangata.finance>
2024-03-04 20:17:58 +0000

    chore: updated types to new prerelease version

--- packages/types/src/interfaces/augment-api-consts.ts
+++ packages/types/src/interfaces/augment-api-consts.ts
@@ -298,6 +298,21 @@ declare module '@polkadot/api-base/types/consts' {
        **/
       [key: string]: Codec;
     };
+    rolldown: {
+      disputePeriodLength: u128 & AugmentedConst<ApiType>;
+      requestsPerBlock: u128 & AugmentedConst<ApiType>;
+      /**
+       * Generic const
+       **/
+      [key: string]: Codec;
+    };
+    sequencerStaking: {
+      minimumSequencers: u32 & AugmentedConst<ApiType>;
+      /**
+       * Generic const
+       **/
+      [key: string]: Codec;
+    };
     system: {
       /**
        * Maximum number of block number to block hash mappings to keep (oldest pruned first).
