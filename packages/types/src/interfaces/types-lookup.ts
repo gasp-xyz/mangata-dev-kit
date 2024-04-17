81e536f7340f005807f317830ecf7d11e187ad90
mangatafinance <solutions@mangata.finance>
2024-04-17 20:33:22 +0000

    chore: updated to new prerelease version

--- packages/types/src/interfaces/types-lookup.ts
+++ packages/types/src/interfaces/types-lookup.ts
@@ -3021,7 +3021,8 @@ declare module '@polkadot/types/lookup' {
     readonly isNotEligibleToBeSequencer: boolean;
     readonly isNotEnoughSequencerStake: boolean;
     readonly isMaxSequencersLimitReached: boolean;
-    readonly type: 'OperationFailed' | 'MathOverflow' | 'SequencerIsNotInActiveSet' | 'SequencerAlreadyInActiveSet' | 'CantUnstakeWhileInActiveSet' | 'NotEligibleToBeSequencer' | 'NotEnoughSequencerStake' | 'MaxSequencersLimitReached';
+    readonly isTestUnstakingError: boolean;
+    readonly type: 'OperationFailed' | 'MathOverflow' | 'SequencerIsNotInActiveSet' | 'SequencerAlreadyInActiveSet' | 'CantUnstakeWhileInActiveSet' | 'NotEligibleToBeSequencer' | 'NotEnoughSequencerStake' | 'MaxSequencersLimitReached' | 'TestUnstakingError';
   }
 
   /** @name SpCoreCryptoKeyTypeId (387) */
